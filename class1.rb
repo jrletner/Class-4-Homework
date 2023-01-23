5fd7019ac1f99fc4cadca4c97a797b0995445021
Joe Letner <jrletner@gmail.com>
2023-01-22 19:04:25 -0600

    Update class1.rb

--- class1.rb
+++ class1.rb
@@ -1,9 +1,15 @@
 puts "Please enter something."
 text = gets.chomp
+
+# split the user's text into individual words
 words = text.split
 
+# create a new hash
 frequencies = Hash.new(0)
 
+# add each word to the hash array and count the number of times
+# the word occurs
 words.each { |word| frequencies[word] += 1 }
 
+# display the hash
 puts frequencies
