e51739559fe38d901f763d62a63441fcc560604c
Joe Letner <jrletner@gmail.com>
2023-01-22 21:06:40 -0600

    Update class2.rb

--- class2.rb
+++ class2.rb
@@ -7,10 +7,12 @@ class Account
 
   # initialze instance variables
   # have the balance have a default value of 100
-  def initialize(name, balance = 100, pin)
+  def initialize(name, type, balance = 100, pin)
     @name = name
+    @type = type
     @balance = balance
     @pin = pin
+    @@Account << self
   end
 
   # create a method to retrieve account balance
@@ -22,14 +24,46 @@ class Account
     end
   end
 
+  # create a method to make a withdrawal
+  def withdraw(pin, amount)
+    if pin == @pin
+      @balance -= amount
+      puts "Hi #{name}, you made a withdrawal of $#{amount} from #{@type}, your balance is now $#{balance}"
+    else
+      pin_error
+    end
+  end
+
+  # create a method to make a deposit
+  def deposit(pin, amount)
+    if pin == @pin
+      @balance += amount
+      puts "Hi #{name}, you made a deposit of $#{amount} into #{@type}, your balance is now $#{balance}"
+    else
+      pin_error
+    end
+  end
+
   # set a message for pin error
   def pin_error
     puts "Wrong PIN number"
   end
+
+  # get all accounts
+  def all
+    @@Account
+  end
 end
 
-joe_checking = Account.new("Joe", 500, 1234)
-joe_checking.get_balance(1234)
+system("clear")
+joe_checking = Account.new("Joe", "Checking", 500, 1234)
+joe_checking.withdraw(1234, 100)
 
-bob_checking = Account.new("Bob", 500, 1234)
-bob_checking.get_balance(1234)
+joe_savings = Account.new("Joe", "Savings", 900, 1234)
+joe_savings.deposit(1234, 1000)
+
+bob_checking = Account.new("Bob", "Checking", 400, 5678)
+bob_checking.deposit(5678, 1000)
+
+bob_savings = Account.new("Bob", "Savings", 900, 5678)
+bob_savings.deposit(5678, 1000)
